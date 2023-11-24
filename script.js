a29561358c822a6cde75f691d59ae072cfddd717
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-11-24 10:29:27 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+// Add an event listener to the button
+document.querySelector('button').addEventListener('click', swapTheme);
+
+function swapTheme() {
+    var mainElement = document.querySelector('main');
+
+    // Toggle between day and night themes
+    if (mainElement.classList.contains('day')) {
+        mainElement.classList.remove('day');
+        mainElement.classList.add('night');
+    } else {
+        mainElement.classList.remove('night');
+        mainElement.classList.add('day');
+    }
+}
+
 
